98837108876893207e579aa544701696e3f3b679
Hemangini Vyas <hemanginivyas3@gmail.com>
2026-01-22 23:35:32 +0530

    fixed

--- userService.ts
+++ userService.ts
@@ -1,17 +1,25 @@
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "./firebase";
 
 export async function getUserProfile(uid: string) {
-  try {
-    const ref = doc(db, "users", uid);
-    const snap = await getDoc(ref);
+  const ref = doc(db, "users", uid);
+  const snap = await getDoc(ref);
 
-    if (!snap.exists()) return null;
-
-    return snap.data();
-  } catch (error) {
-    console.error("getUserProfile error:", error);
-    return null;
-  }
+  if (!snap.exists()) return null;
+  return snap.data();
 }
 
+export async function saveUserProfile(uid: string, name: string) {
+  const ref = doc(db, "users", uid);
+
+  await setDoc(
+    ref,
+    {
+      name,
+      createdAt: serverTimestamp(),
+    },
+    { merge: true }
+  );
+
+  return true;
+}
